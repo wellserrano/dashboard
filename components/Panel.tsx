5051d25f90b4c78146e0bbeb560f1f77e2faf147
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-02-24 14:50:03 -0300

    feat: Panel included in grid

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -1,23 +1,27 @@
-import Image from 'next/image'
 import React from 'react'
 
-import smileyImg from '../public/smiley.svg'
+import Image from 'next/image'
 
-const CardQuality: React.FC = () => {
+const Panel: React.FC = () => {
   return (
-    <div className='flex flex-col items-center text-white bg-[#363447] py-7 px-20 rounded-2xl shadow-card'>
-      <div id="top" className='font-semiboldbold text-2xl'>
-        <p>NPS geral</p>
+    <div className='col-span-3 py-7 px-12 text-white bg-[#363447] rounded-2xl shadow-card'>
+      <h3 className='mb-8 text-2xl font-semibold'>Vendas por dia da semana</h3>
+      <div className='flex justify-between'>
+        <div className='flex flex-col'>
+            <p className='mb-2 text-sm'>
+              Dia com mais vendas
+            </p>
+            <p className='mb-8 text-2xl'>quarta-feira</p>
+            <p className='mb-2 text-sm'>Dia com menos vendas</p>
+            <p className='text-2xl'>domingo</p>
+        </div>
+        <div>
+          <Image src='/chart-test.png' alt='chat example' width={523} height={186}/>
+        </div>
       </div>
-      <div id="content" className='flex flex-col items-center text-[#81FBB8] font-semibold text-2xl my-20 gap-4'>
-        <Image src={ smileyImg } alt="smiley face"/>
-        <p>Excelente!</p>
-      </div>
-      <div id="bottom" className='text-sm font-medium'>
-        <p>NPS Score 75</p>
-      </div>
+
     </div>
   )
 }
 
-export default CardQuality
+export default Panel
